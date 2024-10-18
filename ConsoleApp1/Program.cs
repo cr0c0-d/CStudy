7413c1097aadb4705276ee09f8458d0fbfa8cb99
cr0c0 <hyde69ciel@gmail.com>
2024-10-18 23:21:42 +0900

    #241018 콘솔앱 만들어보기, 클래스 선언

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ConsoleApp1
+{
+    class CurrentTime {
+        public int Number = 0;
+        public DateTime NowTime;
+        public void GetNumber()
+        {
+            Number++;
+        }
+
+        public void OutNumber()
+        {
+            Console.WriteLine("현재 순서는 " + Number);
+        }
+
+        public void GetTime()
+        {
+            NowTime = DateTime.Now; 
+        }
+
+        public void OutTime()
+        {
+            Console.WriteLine("현재 시각은 " + NowTime);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CurrentTime currentTime = new CurrentTime();
+            for (int i = 0; i < 100; i++)
+            {
+                currentTime.GetNumber();
+                currentTime.OutNumber();
+
+                currentTime.GetTime();
+                currentTime.OutTime();
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+
+}
